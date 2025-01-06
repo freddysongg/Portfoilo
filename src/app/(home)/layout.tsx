ad5c13a8cc43af0463101f11e59aca67f395f98f
Freddy Song <fredsong99@gmail.com>
2025-01-06 15:49:11 -0800

    fix: updated resume 1/06/2025

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,10 +1,20 @@
+'use client';
+
+import React from 'react';
 import { Navbar } from '@/components/page-ui/Navbar';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="dark:bg-black">
+    <div className="min-h-screen flex flex-col">
       <Navbar />
-      {children}
+
+      <main className="flex-1 px-4 sm:px-6 md:px-8 lg:px-12">
+        {children}
+      </main>
+
+      <footer className="text-center py-4 text-sm text-neutral-600">
+        © {new Date().getFullYear()} Freddy Song. All Rights Reserved.
+      </footer>
     </div>
   );
 }
